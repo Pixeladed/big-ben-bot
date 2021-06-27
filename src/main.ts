6b6ef12d5b2e679c09415d6a5004a2a06ffd787b
Pixeladed <vietminhle98@gmail.com>
2021-06-27 19:42:41 +1000

    add simple log

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,18 @@ import { config } from './config'
 
 const client = new Client()
 
-client.login(config.BOT_TOKEN)
+const main = async () => {
+  console.log('Logging with BOT_TOKEN')
+  await client.login(config.BOT_TOKEN)
+  console.log('Logged in')
+}
+
+const exit = () => {
+  console.log('Exiting')
+  process.exit(0)
+}
+
+main()
+
+process.on('SIGINT', exit)
+process.on('SIGTERM', exit)
